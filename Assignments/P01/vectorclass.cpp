d5bfd9d9f4242467b040c0c9df7ac17d968f58d1
Collin Franklin <collin.franklin.2003@gmail.com>
2023-02-07 13:29:01 -0600

    Update

--- Assignments/P01/vectorclass.cpp
+++ Assignments/P01/vectorclass.cpp
@@ -29,7 +29,7 @@ class Vector {
         Vector(int);            // specify vector size
         Vector(const Vector &); // copy constructor
         Vector(int *, int);     // create vector given array of items
-        Vector(string fileName);
+        Vector(string fileName);// create a vector given a file
 
         void push(int);
         void print();
@@ -60,7 +60,17 @@ Vector::Vector(const Vector &other) {
     }
 }
 
-Vector::Vector(string fileName) {
+    Vector::Vector(int *A, int size) {
+        int data;
+        Head = Tail = nullptr;
+        for (int i = 0; i < size; i++) {
+            data = A[i];
+            push(data);
+        }
+    }
+
+    Vector::Vector(string fileName)
+{
     int data;
     Head = Tail = nullptr;
     ifstream fin;
@@ -180,3 +190,91 @@ int Vector::popFront() {
     return val;    
 }
 
+int Vector::popRear() {
+    Node *temp = new Node();
+    Node *traverse = new Node();
+    int val;
+    while (traverse->next && traverse->next->data < temp->data)
+    {
+        traverse = traverse->next;
+        if (traverse->next == nullptr)
+            val = traverse->data;
+    }
+    return val;
+}
+
+int Vector::popAt(int loc) {
+    
+}
+
+int main() {
+    // int x = 0;
+
+    // Vector v1;
+    // v1.pushFront(18);
+    // v1.pushFront(20);
+    // v1.pushFront(25);
+    // v1.pushRear(18);
+    // v1.pushRear(20);
+    // v1.pushRear(25);
+    // v1.print();
+    // // [25, 20, 18, 18, 20, 25]
+
+    // int A[] = {11, 25, 33, 47, 51};
+    // Vector v2(A, 5);
+    // v2.print();
+    // // [11, 25, 33, 47, 51]
+
+    // v2.pushFront(9);
+    // // v2.inOrderPush(27);
+    // v2.pushRear(63);
+    // v2.print();
+    // // [9, 11, 25, 33, 47, 51, 63]
+
+    // v1.pushRear(v2);
+    // v1.print();
+    // // [25, 20, 18, 18, 20, 25, 9, 11, 25, 33, 47, 51, 63]
+
+    // x = v1.popFront();
+    // x = v1.popFront();
+    // x = v1.popFront();
+    // v1.print();
+    // // [18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
+    // cout << x << endl;
+    // // 18
+
+    // x = v1.popRear();
+    // x = v1.popRear();
+    // x = v1.popRear();
+    // v1.print();
+    // // [18, 20, 25, 9, 11, 25, 27, 33]
+    // cout << x << endl;
+    // // 47
+
+    // x = v2.popAt(3);
+    // v2.print();
+    // // [9, 11, 25, 33, 47, 51, 63]
+    // cout << x << endl;
+    // // 27
+
+    // x = v2.find(51);
+    // cout << x << endl;
+    // // 5
+
+    // x = v2.find(99);
+    // cout << x << endl;
+    // // -1
+
+    // Vector v3(v1);
+    // v3.print();
+    // // [18, 20, 25, 9, 11, 25, 27, 33]
+
+    // v3.pushFront(v2);
+    // v3.print();
+    // //[9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+
+    // Vector v4("input.dat");
+    // v4.pushRear(v3);
+    // v4.print();
+    // // [56, 61, 97, 66, 83, 25, 26, 11, 53, 49, 62, 18, 10, 18, 14, 3, 4, 23, 18, 24, 26, 27, 54, 14, 12, 45, 65, 98, 56, 97, 15, 84, 98, 9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
+}
