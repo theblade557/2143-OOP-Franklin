fa479444d631aac9774a43937f0920d2e2d023cc
theblade557 <collin.franklin.2003@gmail.com>
2023-03-07 21:11:44 -0600

    Update

--- Assignments/P03/Graphviz.cpp
+++ Assignments/P03/Graphviz.cpp
@@ -23,10 +23,10 @@ public:
      * Very basic print method to print out some graphviz style
      * code defining an edge.
      */
-    string print()
-    {
-        return "[color=" + att["color"] + ",arrow_type=" + att["arrow_type"] + "]";
-    }
+    // string print() {
+    //   return "[color=" + att["color"] + ",arrow_type=" + att["arrow_type"] +
+    //   "]";
+    // }
     /**
      * Update an edge attribute. Assumes key is valid.
      */
@@ -44,17 +44,21 @@ public:
             att[x.first] = x.second;
         }
     }
+
     /**
      * Use our basic print to dump this class out.
      */
     friend ostream &operator<<(ostream &os, Attribute &e)
     {
+
         os << "[";
+
         int i = 0;
         for (auto const &x : e.att)
         {
             os << x.first << "="
                << "\"" << x.second << "\"";
+
             if (i < e.att.size() - 1)
             {
                 os << ", ";
@@ -62,13 +66,51 @@ public:
             i++;
         }
         os << "]";
+
         return os;
     }
+
+    friend fstream &operator<<(fstream &fout, Attribute &e)
+    {
+
+        fout << "[";
+        int i = 0;
+        for (auto const &x : e.att)
+        {
+            fout << x.first << "="
+                 << "\"" << x.second << "\"";
+            if (i < e.att.size() - 1)
+            {
+
+                fout << ", ";
+            }
+            i++;
+        }
+
+        fout << "]";
+        return fout;
+    }
+    // friend fstream &operator<<(fstream &os, Attribute &e) {
+    //   os << "[";
+    //   int i = 0;
+    //   for (auto const &x : e.att) {
+    //     os << x.first << "="
+    //        << "\"" << x.second << "\"";
+    //     if (i < e.att.size() - 1) {
+    //       os << ", ";
+    //     }
+    //     i++;
+    //   }
+    //   os << "]";
+    //   return os;
+    // }
 };
 
 class Edge : public Attribute
 {
     int eid;
+    int start;
+    int end;
 
 public:
     Edge()
@@ -82,6 +124,33 @@ public:
         att["color"] = color;      // default color
         att["arrow_type"] = arrow; // default arrow shape
     }
+    Edge(int start, int end)
+    {
+        this->start = start;
+        this->end = end;
+    }
+
+    friend ostream &operator<<(ostream &os, Edge &e)
+    {
+        os << e.start << "->" << e.end << " ";
+        os << "[";
+
+        int i = 0;
+        for (auto const &x : e.att)
+        {
+            os << x.first << "="
+               << "\"" << x.second << "\"";
+
+            if (i < e.att.size() - 1)
+            {
+                os << ", ";
+            }
+            i++;
+        }
+        os << "]";
+
+        return os;
+    }
 };
 
 class Node : public Attribute
@@ -91,9 +160,23 @@ class Node : public Attribute
 public:
     Node()
     {
-        att["color"] = "black"; // default color
-        att["shape"] = "box";   // default arrow shape
+        att["color"] = "black";  // default color
+        att["shape"] = "record"; // default arrow shape
     }
+
+    Node(map<string, string> atts)
+    {
+        for (auto const &x : atts)
+        {
+            att[x.first] = x.second;
+        }
+    }
+};
+
+struct Shape
+{
+    string shape;
+    Shape(string name) { shape = name; }
 };
 
 class GraphViz
@@ -102,117 +185,228 @@ class GraphViz
     int nodeId;
     vector<Edge> Edges;
     vector<Node> Nodes;
+    string graphName;
+    string graphShape;
 
-    string makeEid(int v, int w) { return to_string(v) + "-" + to_string(w); }
+    //string makeEid(int v, int w) { return to_string(v) + "-" + to_string(w); }
 
 public:
-
-    //GraphViz constructor
     GraphViz()
     {
         edgeId = 0;
         nodeId = 0;
+        graphName = "foo";
     }
 
-    //adds node to the back
+    GraphViz(string graph_name, string graph_shape)
+    {
+        edgeId = 0;
+        nodeId = 0;
+        graphName = graph_name;
+        graphShape = graph_shape;
+    }
+
     int addNode()
     {
         Nodes.push_back(Node());
         return Nodes.size() - 1;
     }
-    
-    //adds an edge to the back
-    void addEdge(int start, int end) { Edges.push_back(Edge()); }
+
+    int addNode(map<string, string> atts)
+    {
+        Nodes.push_back(Node(atts));
+
+        return Nodes.size() - 1;
+    }
+
+    void updateNode(int id, string key, string value)
+    {
+        Nodes[id].addAttribute(key, value);
+    }
+
+    void addEdge(int start, int end)
+    {
+        // make and edge constructor that receives two ints so you
+        // can print them out
+        Edges.push_back(Edge(start, end));
+    }
+
+    void addEdge(int start, int end, map<string, string> att)
+    {
+
+        Edges.push_back(Edge(start, end));
+        Edges[Edges.size() - 1].addAttributes(att);
+    }
 
     // output your graphviz stuff
-    void printGraph(string graphName)
+    void printGraph(string graphName = "")
     {
-        cout << "Digraph" << graphName << "{";
+        if (graphName.size() == 0)
+            graphName = this->graphName;
+
+        cout << "digraph" << graphName << "{";
         // loop over nodes and print them
+        for (int i = 0; i < Nodes.size(); i++)
+        {
+            cout << i << " " << Nodes[i] << endl;
+        }
         // loop over edges and print them
         cout << "}\n";
+    }
+
+    friend ostream &operator<<(ostream &banana, GraphViz &g)
+    {
+        for (int i = 0; i < g.Nodes.size(); i++)
+        {
+            banana << i << " " << g.Nodes[i] << endl;
+        }
+        for (int i = 0; i < g.Edges.size(); i++)
+        {
+            banana << g.Edges[i] << endl;
+        }
+        return banana;
     }
 };
 
 int main()
 {
-    // add and save nodes and edges
+    // create graphviz instances with specific shapes
+    GraphViz G("LinkedList", "LL");
+    GraphViz D("DoublyLinkedList", "DLL");
+    GraphViz B("BinarySearchTree", "BST");
 
-    ofstream fout;
-    fout.open("test.out");
-    printHeading(fout);
+    // used as an example to load attributes for a node
+    map<string, string> Box;
+    map<string, string> Arrow;
+    // example attribute that gets a map of attributes and
+    // adds them to "A"
+    Attribute A;
+    Box["shape"] = "box";
+    Box["color"] = "blue";
+    Box["opacity"] = "0.8";
+    A.addAttributes(Box);
 
-    int start;
-    int end;
+    Arrow["color"] = "black";
+    Arrow["arrow_type"] = "vee";
+    A.addAttributes(Arrow);
 
-    Edge e;
-    map<string, string> stuff;
+    // Creating nodes in graphviz and sending in default
+    // colors / shapes, etc.
+    int nodeId = G.addNode(Box);
+    nodeId = G.addNode(Box);
+    G.updateNode(nodeId, "shape", "circle");
+    G.updateNode(nodeId, "color", "red");
+    nodeId = G.addNode(Box);
+    nodeId = G.addNode(Box);
+    nodeId = G.addNode(Box);
 
-    stuff["fillcolor"] = "red";
-    stuff["nodecolor"] = "purple";
-    stuff["edgestyle"] = "dashed";
+    G.addEdge(0, 1, Arrow);
+    G.addEdge(1, 2, Arrow);
+    G.addEdge(2, 3, Arrow);
+    G.addEdge(3, 4, Arrow);
 
-    vector<Node> Nodes;
+    cout << G << endl;
 
-    // index = 0-3
-    Nodes.push_back(Node());
-    Nodes.push_back(Node());
-    Nodes.push_back(Node());
-    Nodes.push_back(Node());
+    // making the doubly linked list
+    // and sending in default colors and shapes
+    nodeId = D.addNode(Box);
+    nodeId = D.addNode(Box);
+    nodeId = D.addNode(Box);
+    nodeId = D.addNode(Box);
+    nodeId = D.addNode(Box);
 
-    // adding attributes
-    Nodes[1].addAttribute("fillColor", "blue");
-    Nodes[2].addAttribute("fillColor", "purple");
-    Nodes[3].addAttribute("fillColor", "red");
+    D.addEdge(0, 1, Arrow);
+    D.addEdge(1, 0, Arrow);
+    D.addEdge(1, 2, Arrow);
+    D.addEdge(2, 1, Arrow);
+    D.addEdge(2, 3, Arrow);
+    D.addEdge(3, 2, Arrow);
+    D.addEdge(3, 4, Arrow);
+    D.addEdge(4, 3, Arrow);
 
-    //printing out graphviz stuff
-    cout << "digraph"
-         << " linkedlist"
-         << "{\n";
+    cout << D << endl;
 
-    fout << "digraph"
-         << " linkedlist"
-         << "{\n";
+    nodeId = B.addNode(Box);
+    nodeId = B.addNode(Box);
+    nodeId = B.addNode(Box);
+    nodeId = B.addNode(Box);
+    nodeId = B.addNode(Box);
+    nodeId = B.addNode(Box);
+    nodeId = B.addNode(Box);
 
-    cout << "rankdir=LR" << endl;
-    fout << "rankdir=LR" << endl;
+    B.addEdge(0, 1, Arrow);
+    B.addEdge(0, 2, Arrow);
+    B.addEdge(1, 3, Arrow);
+    B.addEdge(1, 4, Arrow);
+    B.addEdge(2, 5, Arrow);
+    B.addEdge(2, 6, Arrow);
 
-    for (int i = 0; i < Nodes.size(); i++)
-    {
-        cout << "\t" << i << " " << Nodes[i] << endl;
-        fout << "\t" << i << " " << Nodes[i] << endl;
-    }
-    cout << "}\n";
-    fout << "}\n";
+    cout << B << endl;
 
-    //   cout<<"=================="<<endl;
-    //   cout<<e<<endl;
-    // cout<<"=================="<<endl;
-
-    //   GraphViz g;
-
-    //   start = g.addNode();
-    //   g.updateNode(id,key,value);
-
-    // g.addEdge(0, 1);
-    // g.addEdge(0, 4);
-    // g.addEdge(1, 2);
-    // g.addEdge(1, 3);
-    // g.addEdge(1, 4);
-    // g.addEdge(2, 3);
-    // g.addEdge(3, 4);
-    // g.addEdge(9, 10);
-    // g.addEdge(4, 9);
-
-    // g.printGraph();
-    return 0;
+    // ofstream fout;
+    // fout.open("sonofabitch.com");
+    // fout<<A<<endl;
 }
+// add and save nodes and edges
 
-//prints heading
-void printHeading(ofstream &fout)
-{
-    fout << "Collin Franklin" << endl;
-    fout << "March 2nd, 2023" << endl;
-    fout << "Spring 2143" << endl;
-    fout << endl;
-}
+//   ofstream fout;
+//   fout.open("test.out");
+//   printHeading(fout);
+
+//   int start;
+//   int end;
+
+//   Edge e;
+//   map<string, string> stuff;
+
+//   stuff["fillcolor"] = "red";
+//   stuff["nodecolor"] = "purple";
+//   stuff["edgestyle"] = "dashed";
+
+//   vector<Node> Nodes;
+
+//   // index = 0-3
+//   Nodes.push_back(Node());
+//   Nodes.push_back(Node());
+//   Nodes.push_back(Node());
+//   Nodes.push_back(Node());
+
+//   Nodes[2].addAttribute("fillColor","purple");
+//   Nodes[3].addAttribute("fillColor", "red");
+
+//   cout << "digraph" << " someGraph" << "{\n";
+
+//   for(int i=0;i<Nodes.size();i++){
+//     cout<<"\t"<<i<<" "<<Nodes[i]<<endl;
+//   }
+//   cout << "}\n";
+
+//   //   cout<<"=================="<<endl;
+//   //   cout<<e<<endl;
+//   // cout<<"=================="<<endl;
+
+//   //   GraphViz g;
+
+//   //   start = g.addNode();
+//   //   g.updateNode(id,key,value);
+
+//   // g.addEdge(0, 1);
+//   // g.addEdge(0, 4);
+//   // g.addEdge(1, 2);
+//   // g.addEdge(1, 3);
+//   // g.addEdge(1, 4);
+//   // g.addEdge(2, 3);
+//   // g.addEdge(3, 4);
+//   // g.addEdge(9, 10);
+//   // g.addEdge(4, 9);
+
+//   // g.printGraph();
+//   return 0;
+// }
+
+// void printHeading(ofstream &fout) {
+//   fout << "Collin Franklin" << endl;
+//   fout << "March 2nd, 2023" << endl;
+//   fout << "Spring 2143" << endl;
+//   fout << endl;
+// }
